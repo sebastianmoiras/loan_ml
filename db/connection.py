bc9299a92e3077c5704686e2e61d1f6e539fa53d
sebastianmoiras <Jonathansebastian858@gmail.com>
2025-11-18 16:45:14 +0700

    Create connection.py

---
+++ db/connection.py
@@ -0,0 +1,16 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_connection():
+    try:
+        conn = psycopg2.connect(
+            os.getenv("DATABASE_URL"),
+            sslmode="require"   
+        )
+        return conn
+    except Exception as e:
+        print("Gagal koneksi ke database:", e)
+        raise
